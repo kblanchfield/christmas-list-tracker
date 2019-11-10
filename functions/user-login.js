633a82f79dec94924c003817037bed8462e08b45
kblanchfield <kateblanchfield@live.co.uk>
2019-11-10 19:49:54 +0100

    remove logging because it works! 🥳 🌟 🚀

--- functions/user-login.js
+++ functions/user-login.js
@@ -5,8 +5,6 @@ const { MG_URL, MG_DB } = process.env
 exports.handler = async (event, context) => {
     context.callbackWaitsForEmptyEventLoop = false
     const { username, password } = JSON.parse(event.body)
-    console.log(username, password)
-    console.log(MG_URL, MG_DB)
 
     try {
         const client = new MongoClient(MG_URL, { useUnifiedTopology: true })
@@ -17,7 +15,6 @@ exports.handler = async (event, context) => {
         const User = db.collection('users')
 
         const user = await User.findOne({ username: username })
-        console.log("User found: ", user)
         client.close()
 
         if (user && bcrypt.compareSync(password, user.password)) {
