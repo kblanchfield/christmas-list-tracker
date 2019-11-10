c1d46508b5d779ceef7d316629957409fa1dc28c
kblanchfield <kateblanchfield@live.co.uk>
2019-11-10 18:34:45 +0100

    don't use styled components

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,14 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const ErrorMessage = styled.p`
-  text-align: center;
-  margin-top: 10px;
-  color: #ff0000;
-`;
 
 const ErrorMessageContainer = ({ errorMessage }) => {
-  return <ErrorMessage>{errorMessage}</ErrorMessage>
+  return <p>{errorMessage}</p>
 }
 
 export default ErrorMessageContainer
